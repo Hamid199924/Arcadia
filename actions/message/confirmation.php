b545a75ea7b7cd68121e31e1b412e78f77f51ed4
Hamid2024 <hamidmahamat04@gmail.com>
2024-11-21 19:03:40 +0100

    Sauvegarder les modifications index.css

--- actions/message/confirmation.php
+++
@@ -1,9 +0,0 @@
-<?php include '../../template/header.php'; ?>
-
-<div class="confirmation-message">
-    <h1>Merci !</h1>
-    <p>Votre message a été envoyé avec succès. Nous vous contacterons bientôt.</p>
-    <a href="../../public/Formulaire.php">Retour au formulaire</a>
-</div>
-
-<?php include '../../template/footer.php'; ?>
